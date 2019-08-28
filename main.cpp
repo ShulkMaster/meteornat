a8064e2e5b782a7ae78bde2cfc83d4f2bbe2bbd1
shulkmaster <00088116@uca.edu.sv>
2019-08-27 18:32:31 -0600

    code debugging for merge sort

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 
 const int size = 8;
 
-int data[size] = {};
+int data[size] = {2, 1, 4, 3, 6, 5, 8, 7};
 
 //Aux functions for formatting, printing and allocating arrays
 
@@ -22,6 +22,8 @@ int *copyArray(const int *original);
 void swap(int *array, int i, int j);
 
 void merge(int *array, int ak, int half, int bk);
+
+void printArray(int *array);
 
 void printArray(int *array, int scale);
 
@@ -39,7 +41,8 @@ void mergeSort(int *array, int start, int range, bool descendent);
 
 int main() {
     int scale = 1;
-    fillArray();
+
+    //fillArray();
 
     printSortArray(data, [](int *dataset) -> void {
                        bubbleSort(dataset, false);
@@ -59,16 +62,28 @@ int main() {
                        mergeSort(dataset, 0, size, false);
                    },
                    scale);
+
+
     return 0;
+}
+
+void printArray(int *array) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << ", ";
+        if ((i + 1) % 10 == 0) {
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
 }
 
 void printArray(int *array, int scale) {
     double step = 10 * scale;
     for (int i = 0; i < step; ++i) {
-        int x = (int) std::ceil(size * (i / step));
+        int x = (int) std::ceil(size / step * i);
         //std::cout << "[" << x << "] = " << array[x] << ", ";
         std::cout << array[x] << ", ";
-        if (i % 10 == 0) {
+        if ((i + 1) % 10 == 0) {
             std::cout << std::endl;
         }
     }
@@ -79,10 +94,12 @@ void printSortArray(int *array, const std::function<void(int *)> &sort, int scal
     int *unsorted = copyArray(array);
     std::cout << "=========================================================================" << std::endl;
     std::cout << "before:" << std::endl;
-    printArray(unsorted, scale);
+    //printArray(unsorted, scale);
+    printArray(unsorted);
     sort(unsorted);
     std::cout << "After:" << std::endl;
-    printArray(unsorted, scale);
+    printArray(unsorted);
+    //printArray(unsorted, scale);
     std::cout << "=========================================================================" << std::endl;
     delete[] unsorted;
 }
@@ -112,17 +129,18 @@ void swap(int *array, int i, int j) {
 void merge(int *array, int ak, int half, int bk) {
     int *temp = new int[bk];
     int stackPointer = 0;
+    int divisorPointer = ak+half+1;
     while (stackPointer < bk) {
-        if (array[ak] <= array[half]) {
+        if (ak <= half and array[ak] <= array[divisorPointer]) {
             temp[stackPointer] = array[ak];
             ak++;
         } else {
-            temp[stackPointer] = array[half];
-            half++;
+            temp[stackPointer] = array[divisorPointer];
+            divisorPointer++;
         }
         stackPointer++;
     }
-    for (int i = ak; i < bk; ++i) {
+    for (int i = 0; i < bk; ++i) {
         array[i] = temp[i];
     }
     delete[] temp;
@@ -186,11 +204,11 @@ void insertionSort(int *array, bool descendent) {
 
 void mergeSort(int *array, int start, int range, bool descendent) {
     if (range <= 2) {
-        if (array[start] > array[start+1]) swap(array, start, start+1);
+        if (array[start] > array[start + 1]) swap(array, start, start + 1);
         return;
     }
     int half = floor(range / 2);
     mergeSort(array, start, half, descendent);
-    mergeSort(array, half + 1, range-half, descendent);
+    mergeSort(array, start + half, range - half, descendent);
     merge(array, start, half, range);
 }
